1cb5b83ad240cbda841cfa18f31a7d1fa49375a8
charansujeev <charansujeev123@gmail.com>
2025-12-08 11:20:33 -0500

    Update DriverFactory to run headless for CI

--- src/test/java/ui/DriverFactory.java
+++ src/test/java/ui/DriverFactory.java
@@ -2,6 +2,7 @@ package ui;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverFactory {
 
@@ -10,8 +11,17 @@ public class DriverFactory {
     // Initialize driver
     public static void initDriver() {
         if (driver == null) {
-            // Make sure ChromeDriver path is set in system PATH, or use WebDriverManager
-            driver = new ChromeDriver();
+            // Set Chrome options
+            ChromeOptions options = new ChromeOptions();
+
+            // Run in headless mode for CI
+            options.addArguments("--headless=new");  // for latest Chrome
+            options.addArguments("--disable-gpu");
+            options.addArguments("--window-size=1920,1080");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+
+            driver = new ChromeDriver(options);
             driver.manage().window().maximize();
         }
     }
